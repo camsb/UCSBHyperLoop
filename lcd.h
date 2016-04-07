7850b29eb5b102f898df6786d580c8fb8cd2799c
camsb <cameron@umail.ucsb.edu>
2016-04-07 12:41:01 -0700

    cam/bh: Testing LCD screen

--- lcd.h
+++ lcd.h
@@ -1,6 +1,79 @@
 #ifndef LCD_H_
 #define LCD_H_
 
+#include <cr_section_macros.h>
+#include <stdio.h>
+#include <stdlib.h>
 
+//////////////////////////////////////////////////////////////////////////////////////////////////////////
+// macros, not modified
+#define EPIN            10
+#define HIGH            1
+#define LOW             0
+#define NULLCHAR        '\0'
+#define NUMOFDATAPINS   11
+#define NUMOFCOLS       17
+#define NUMOFINITSTEPS  10
+#define NUMOFITERATIONS NUMOFCOLS * NUMOFROWS
+#define NUMOFROWS       4
+#define PRINTDEBUG      FALSE
+#define RSPIN           8
+#define RWPIN           9
+#define SEVENTEENSPACES "                "    // initialize each array to be all spaces
+#define SPACE           32
+#define TICKRATE_HZ1    200                   // Not sure the ticks per second, but can go higher to be instant, ex: 1000
+#define TICKRATE_HZ2    2                     // 1 number of ticks per second
+
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////
+// structs
+struct Lines {
+  int   row;                 // the line number
+  int   column;              // points to the current letter to be printed
+  char* lines[ NUMOFROWS ];  // array of 4 pointers to chars
+};
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////
+// arrays, not modified
+//int  DATAPINS[]         = {    3,   14,   22,   27,   25,   15,   23,   26,   20,   24,   28, }; // need to keep in correct order
+//int  DATAPORTS[]        = {    5,    2,    2,    2,    2,    2,    2,    2,    1,    1,    1, }; // ports with matching pins
+int  DATAPINS[]         = {    3,    4,    5,    6,    7,    8,    9,   10,    0,    1,    2, }; // need to keep in correct order
+int  DATAPORTS[]        = {    3,    3,    3,    3,    3,    3,    3,    3,    3,    3,    3, }; // ports with matching pins
+char INITVALUES[]       = { 0x30, 0x30, 0x30, 0x38, 0x10, 0xC0, 0x06, 0x01, 0x0F, 0x0D };        // hex values of initialization commands for LCD
+char LINES[]            = { 0x80, 0xC0, 0x90, 0xD0 };                                            // start location of each line
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////
+// globals, are modified
+int    CharCount        = 0;
+int    InitCount        = 0;
+int    InterruptCount   = 0;
+int    IsNewData        = TRUE;
+struct Lines* L         = NULL;
+int    TIMER1COUNT      = 0;
+
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////
+// function declarations
+void command( int v );
+char convertChar( char c );
+void flipE();
+void incrementColumn();
+void incrementRow();
+void lcdInit();
+void linesDeInit();
+void linesInit();
+void nextChar();
+void nextInit();
+void setData( int v );
+void setDataPinsToOutputAndLow();
+void setLine( int lineNum, char* str );
+void setRS(   int v );
+void setRW(   int v );
+void timer0Init();
+void timer1Init();
+void write(   int v );
+
+void TIMER0_IRQHandler();
+void TIMER1_IRQHandler();
 
 #endif /* LCD_H_ */
--- main.c
+++ main.c
@@ -11,55 +11,90 @@
 #include "stdlib.h"
 #include "accelerometer.h"
 #include "gyroscope.h"
+#include "lcd.h"
 
-int main(void)
-{
+int main(void) {
+
+#if defined (__USE_LPCOPEN)
+#if !defined(NO_BOARD_LIB)
+    // Read clock settings and update SystemCoreClock variable
     SystemCoreClockUpdate();
     // Set up and initialize all required blocks and
     // functions related to the board hardware
     Board_Init();
-    printf("Celeste is SSOOOO lame! -Bucky\n");
-    uint32_t            temperature;
-    uint32_t            pressure;
-    float*          acceleration;
-    float*          rotation;
-    struct constants    *c;
+#endif
+#endif
+    lcdInit();
 
-    c = ( struct constants * ) calloc( 1, sizeof(struct constants) );
+    struct Lines lines;
+    L = &lines;
 
-    i2cInit();
+    linesInit();
+    setLine( 0, "012345678910" );
+    setLine( 1, "abcdefghijklm" );
+    setLine( 2, "nopqrstuvwxyz" );
+    setLine( 3, "~!@#$%^&*()_+-=" );
 
-    initCalibrationData( c );
     timer0Init();
+    timer1Init();
 
-    //////////////////////////////////////////////////////////////////
-    // get temperature
-//  temperature = getTemperature( c );
-//  DEBUGOUT( "temperature = %u\n", temperature );
-
-    //////////////////////////////////////////////////////////////////
-    // get pressure
-//  pressure = getPressure( c );
-//  DEBUGOUT( "pressure = %u\n", pressure );
-
-    //////////////////////////////////////////////////////////////////
-    // get acceleration
-//  while(1) {
-//          acceleration = getAcceleration();
-//          delay(1);
-//  }
-    while( 1 )
-    {
-        acceleration = getAcceleration();
-        rotation = getRotAcceleration();
-
-        delay( 100 );
+    while(1) {  // infinte loop
+        __WFI();
     }
-    Chip_I2C_DeInit( I2C0 );
-    timer0DeInit();
 
-    free( c );
-    //Chip_I2C_DeInit(I2C1);
-
+    // Currently will never reach this line because of the infinite loop
+    linesDeInit();
     return 0;
 }
+
+// int main(void)
+// {
+//     SystemCoreClockUpdate();
+//     // Set up and initialize all required blocks and
+//     // functions related to the board hardware
+//     Board_Init();
+//     printf("Celeste is SSOOOO lame! -Bucky\n");
+//     uint32_t            temperature;
+//     uint32_t            pressure;
+//     float*          acceleration;
+//     float*          rotation;
+//     struct constants    *c;
+
+//     c = ( struct constants * ) calloc( 1, sizeof(struct constants) );
+
+//     i2cInit();
+
+//     initCalibrationData( c );
+//     timer0Init();
+
+//     //////////////////////////////////////////////////////////////////
+//     // get temperature
+// //  temperature = getTemperature( c );
+// //  DEBUGOUT( "temperature = %u\n", temperature );
+
+//     //////////////////////////////////////////////////////////////////
+//     // get pressure
+// //  pressure = getPressure( c );
+// //  DEBUGOUT( "pressure = %u\n", pressure );
+
+//     //////////////////////////////////////////////////////////////////
+//     // get acceleration
+// //  while(1) {
+// //          acceleration = getAcceleration();
+// //          delay(1);
+// //  }
+//     while( 1 )
+//     {
+//         acceleration = getAcceleration();
+//         rotation = getRotAcceleration();
+
+//         delay( 100 );
+//     }
+//     Chip_I2C_DeInit( I2C0 );
+//     timer0DeInit();
+
+//     free( c );
+//     //Chip_I2C_DeInit(I2C1);
+
+//     return 0;
+// }
