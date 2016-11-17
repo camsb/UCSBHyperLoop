13ba9aab059e3739dc6b81d850088224748d2140
Tristan Seroff <Tristan.Seroff@gmail.com>
2016-11-17 14:39:06 -0800

    Fix printouts, add BMS_ACTIVE define

--- src/bms.c
+++ src/bms.c
@@ -1,3 +1,6 @@
+// BMS Interfacing library.
+// BMS needs 'boot' button to be pressed before it will talk over I2C!
+
 #include "bms.h"
 #include "time.h"
 #include "i2c.h"
@@ -5,28 +8,28 @@
 
 #define I2CADDRESS 0x08
 #define TEMP_REG_1 0x2C
-#define TEMP_REF_2 0x2D
+#define TEMP_REG_2 0x2D
 #define CELL_1_VOLT_REG_1 0x0C
 #define CELL_1_VOLT_REG_2 0x0D
 
-// Check this.
 #define I2CINTERFACE I2C0
 
-// Pinouts for I2C0
-// SDA -
-// SCL -
+// I2C0 located on J174
+// SCL = pin 1
+// SDA = pin 2
+// GND = pin 3
+// 3.3V = pin 4
 
 void testBMS(){
     // Do all the things.
 
 	// Read the chip die temperature
 	int temp = readBMSRegs(I2CINTERFACE, I2CADDRESS, TEMP_REG_1, TEMP_REG_2);
-	printf("Temp is: " + temp);
+	printf("Temp is: %d \n", temp);
 
 	int volt = readBMSRegs(I2CINTERFACE, I2CADDRESS, CELL_1_VOLT_REG_1, CELL_1_VOLT_REG_2);
-	printf("Voltage of cell 1 is: " + volt);
+	printf("Voltage of cell 1 is: %d \n", volt);
 }
-
 
 // Read 2-byte register, taken from temp_press.c
 uint16_t readBMSRegs(I2C_ID_T id, uint8_t slaveAddr, uint8_t msbReg, uint8_t lsbReg)
