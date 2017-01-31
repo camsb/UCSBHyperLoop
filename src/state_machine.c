3a34d805982aae07cb961376d06b7d7e6f6fc544
Brian Canty <bialeusk@gmail.com>
2017-01-30 21:12:07 -0800

    Update state_machine.c

--- src/state_machine.c
+++ src/state_machine.c
@@ -3,12 +3,13 @@
 
 /* Hyperloop class -----------------------------------------------------*/
 typedef struct HyperloopTag {
-    QHsm super;                                        /* extend class QHsm */
-                                             /* extended state variables... */
-    uint8_t foo;
+    QHsm super;                                        	 /* extend class QHsm */                                         
+    uint8_t engine_throttle[NUM_ENGINES];		 /* extended state variables... */
+    uint8_t engine_flag = 0;
+    uint8_t brake_flag = 0;
 } Hyperloop;
 
-void   Hyperloop_ctor(void);                                /* the ctor */
+void   Hyperloop_ctor(void);                             /* the ctor */
 static QState Hyperloop_initial(Hyperloop *me);          /* initial handler */
 static QState Hyperloop_stationary(Hyperloop *me);       /* state handler */
 static QState Hyperloop_idle(Hyperloop *me);            
@@ -19,7 +20,7 @@ static QState Hyperloop_rev_engines(Hyperloop *me);
 static QState Hyperloop_power_down(Hyperloop *me);
 //static QState Hyperloop_hovering(Hyperloop *me);
 static QState Hyperloop_hover(Hyperloop *me);
-static QState Hyperloop_braking(Hyperloop *me);             
+static QState Hyperloop_braking(Hyperloop *me);
 
 /* global objects ----------------------------------------------------------*/
 Hyperloop HSM_Hyperloop;              /* the sole instance of the QHsmTst HSM */
@@ -27,7 +28,10 @@ Hyperloop HSM_Hyperloop;              /* the sole instance of the QHsmTst HSM */
 /*..........................................................................*/
 void Hyperloop_ctor(void) {
     QHsm_ctor(&HSM_Hyperloop.super, (QStateHandler)&Hyperloop_initial);
-    HSM_Hyperloop.foo = 0;              /* initialize extended state variable */
+    HSM_Hyperloop.brake_flag = 0;              /* initialize extended state variable */
+    HSM_Hyperloop.engine_flag = 0;
+    for(int i = 0; i < NUM_ENGINES; i++)
+	    HSM_Hyperloop.engine_throttle[i] = 0;
 }
 
 /*..........................................................................*/
@@ -40,6 +44,10 @@ QState Hyperloop_stationary(Hyperloop *me) {
     switch (Q_SIG(me)) {
         case Q_ENTRY_SIG: {
             BSP_display("stationary-ENTRY;");
+	    HSM_Hyperloop.brake_flag = 0;
+    	    HSM_Hyperloop.engine_flag = 0;
+	    for(int i = 0; i < NUM_ENGINES; i++)
+	    	HSM_Hyperloop.engine_throttle[i] = 0;
             return Q_HANDLED();
         }
         case Q_EXIT_SIG: {
@@ -139,10 +147,12 @@ QState Hyperloop_engines_on(Hyperloop *me) {
     switch (Q_SIG(me)) {
         case Q_ENTRY_SIG: {
             BSP_display("engines_on-ENTRY;");
+	    HSM_Hyperloop.engine_flag = 1;
             return Q_HANDLED();
         }
         case Q_EXIT_SIG: {
             BSP_display("engines_on-EXIT;");
+	    HSM_Hyperloop.engine_flag = 0;
             return Q_HANDLED();
         }
         case Q_INIT_SIG: {
@@ -158,6 +168,8 @@ QState Hyperloop_rev_engines(Hyperloop *me) {
         case Q_ENTRY_SIG: {
             BSP_display("rev_engines-ENTRY;");
             BSP_display("revving engines;");
+	    for(int i = 0; i < NUM_ENGINES; i++)
+	    	HSM_Hyperloop.engine_throttle[i] = 10;
             return Q_HANDLED();
         }
         case Q_EXIT_SIG: {
@@ -248,6 +260,7 @@ QState Hyperloop_braking(Hyperloop *me) {
         case Q_ENTRY_SIG: {
             BSP_display("braking-ENTRY;");
             BSP_display("engaging brakes;");
+	    HSM_Hyperloop.brake_flag = 0; 
             return Q_HANDLED();
         }
         case Q_EXIT_SIG: {
